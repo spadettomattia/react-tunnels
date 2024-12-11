f26ef24c3a60a9b719cf2d4e0d6d8f40ac6a0814
Mattia Spadetto <mattia.spadetto@smc.it>
2024-12-11 10:16:32 +0100

    [index] Update default 'useTunnelState' export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as Tunnel } from './Tunnel'
 export { default as TunnelPlaceholder } from './TunnelPlaceholder'
 export { default as TunnelProvider } from './TunnelProvider'
-export { default as TunnelState } from './TunnelState'
+export { default as useTunnelState } from './useTunnelState'
